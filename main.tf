53eb06dbf21389695c162a6605be4eced7ad1338
Akinola Ojuola <aojuola@cloudhight.com>
2023-08-03 22:19:04 +0100

    Adding Infra code

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "aws_security_group" "web_server_sg" {
   vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

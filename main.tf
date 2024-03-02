5aac5ad4fcd945abcdf048ae40f1bb22942d025f
eamanze <85456647+eamanze@users.noreply.github.com>
2024-03-02 09:46:06 +0100

    update on mai.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
-provider "aws" {
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
 
+  cloud {
+    organization = "eamanze-test"
+
+    workspaces {
+      name = "demo-github-actions"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
 }
 
 resource "aws_vpc" "my_vpc" {
